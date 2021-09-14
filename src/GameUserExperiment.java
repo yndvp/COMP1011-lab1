466b85d5bf9027a8594911966448875024fae41f
yndvp <58580623+yndvp@users.noreply.github.com>
2021-09-14 19:20:20 +0900

    Initial commit

---
+++ src/GameUserExperiment.java
@@ -0,0 +1,8 @@
+public class GameUserExperiment {
+    public static void main(String[] args){
+        GameUser user1 = new GameUser("Yunna", "Widowmaker", 12, 35.6);
+        GameUser user2 = new GameUser("Steven", "Mei", 53, 105.3);
+        System.out.println(user1);
+        System.out.println(user2);
+    }
+}
